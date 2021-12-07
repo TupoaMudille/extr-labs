3afbbcc67a57bb5850db2d3685ff48121df15aa9
TupoaMudille <71610105+TupoaMudille@users.noreply.github.com>
2021-12-07 14:12:20 +0300

    Update main.cpp

--- лаба1/лаба1/main.cpp
+++ лаба1/лаба1/main.cpp
@@ -16,8 +16,8 @@ int main(int argc, char* argv[])
 	TSet obj3 = obj2;
 	obj3.print_array();
 
-	TSet obj5(14);
-	cout<<(obj5<<5);//сдвиг на 5
+	TSet obj5(5);
+	cout<<(obj5<<5);//sdvig 5
 
 	if (obj3 == obj2) std::printf("true");
 	else std::printf("false");
@@ -26,8 +26,8 @@ int main(int argc, char* argv[])
 	else std::printf("false");
 	if (obj1 == obj2) std::printf("true");
 	else std::printf("false");
-	TBitField  Elem1(12);  //инициализируем числом, а оно в конструкторе преобразуется в биты
-    Elem1.print_array();   //результат выводим на экран,  первый элемент справа, последний слева
+	TBitField  Elem1(12); 
+    Elem1.print_array();   
 	system("pause");
 	return 0;
 };
